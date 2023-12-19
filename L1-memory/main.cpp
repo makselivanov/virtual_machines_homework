4a62fbd9c2aa12cf9a1802fb47334b90651bd786
Makar Selivanov <makar.selivanov@gmail.com>
2023-12-20 02:06:19 +0300

    working prototype for hw1

--- L1-memory/main.cpp
+++ L1-memory/main.cpp
@@ -7,9 +7,9 @@
 /**
  * | Измерение                          | lscpu --cache | измеренные |
  * | ---------------------------------- | ------------- | ---------- |
- * | Размер кеша одного ядра (ONE-SIZE) | 48 KiB        |            |
- * | Размер линии (COHERENCY-SIZE)      | 64 B          |            |
- * | Ассоциативность (WAYS)             | 12            |            |
+ * | Размер кеша одного ядра (ONE-SIZE) | 48 KiB        | 49152      |
+ * | Размер линии (COHERENCY-SIZE)      | 64 B          | 64         |
+ * | Ассоциативность (WAYS)             | 12            | 12         |
  */
 struct info_L1 {
     size_t cache_size;
@@ -28,6 +28,7 @@ struct measured_time {
     std::chrono::duration<double, std::nano> total;
     double nano_per_loop;
 };
+
 const size_t DEFAULT_LOOP_SIZE = 1 << 20;
 const size_t MAX_SIZE = 1 << 23;
 const double TIME_THRESHOLD = 1.2;
@@ -35,7 +36,8 @@ const size_t MAX_ASSOC = 128;
 const size_t MIN_ASSOC = 4;
 const size_t MIN_WAY_SIZE = 1 << 8; //16
 const size_t MAX_WAY_SIZE = 1 << 20;
-const size_t EPOCHS = 20;
+const size_t EPOCHS = 5;
+const size_t MIN_CACHE_LINE = 8;
 std::random_device dev;
 std::mt19937 rng(dev());
 
@@ -65,7 +67,7 @@ void generate_test_array(size_t size, size_t step) {
     step /= sizeof(uint32_t);
     std::vector<int> buffer(size);
     std::iota(buffer.begin(), buffer.end(), 0);
-    std::shuffle(buffer.begin()+1, buffer.end(), rng);
+    std::shuffle(buffer.begin() + 1, buffer.end(), rng);
     for (size_t i = 0; i < size; ++i) {
         array[buffer[i] * step] = buffer[(i + 1) % size] * step;
     }
@@ -110,6 +112,44 @@ info_L1 get_cache_size_and_associativity() {
     return {.cache_size = cache_size, .associativity = assoc};
 }
 
+size_t generate_test_array_for_line(size_t cache_size, size_t associativity, size_t cache_line) {
+    size_t indices = cache_size / associativity / cache_line;
+    size_t cache_line_uint32 = cache_line / sizeof(uint32_t);
+    size_t size = indices * associativity * cache_line_uint32;
+    std::vector<uint32_t> buffer(size);
+    for (size_t i = 0; i < indices; ++i) {
+        for (size_t j = 0; j < associativity; ++j) {
+            for (size_t k = 0; k < cache_line_uint32; ++k) {
+                size_t index = k + cache_line_uint32 * (j + i * associativity);
+                size_t offset = (j + i * associativity) * cache_size / associativity;
+                buffer[index] = k + (i * cache_line + offset) / sizeof(uint32_t);
+            }
+        }
+    }
+    std::shuffle(buffer.begin() + 1, buffer.end(), rng);
+    for (size_t i = 0; i < size; ++i) {
+        array[buffer[i]] = buffer[(i + 1) % size];
+    }
+    return size;
+}
+
+info_L1 get_cache_line(size_t cache_size, size_t associativity) {
+    info_L1 result = {.cache_size = cache_size, .cache_line = 0, .associativity = associativity};
+    measured_time prev_time{};
+    for (int cache_line = MIN_CACHE_LINE; cache_line <= cache_size / associativity; cache_line *= 2) {
+        size_t len = generate_test_array_for_line(cache_size, associativity, cache_line);
+        measured_time time = measure(len);
+        fprintf(stderr, "%d, %f, %f\n", cache_line, time.total.count(), time.nano_per_loop);
+        if (cache_line != MIN_CACHE_LINE && prev_time.nano_per_loop > TIME_THRESHOLD * time.nano_per_loop) {
+            result.cache_line = cache_line * sizeof(uint32_t);
+            return result;
+        }
+        prev_time = time;
+    }
+
+    return result;
+}
+
 info_L1 get_info_about_L1_memory() {
     std::map<info_L1, size_t> counter;
     fprintf(stderr, "Calculating cache size and associativity\n");
@@ -122,6 +162,20 @@ info_L1 get_info_about_L1_memory() {
     size_t max_count = 0;
     for (auto [measure, count]: counter) {
         if (count > max_count) {
+            max_count = count;
+            result = measure;
+        }
+    }
+    counter.clear();
+    fprintf(stderr, "Calculating cache line\n");
+    for (int epoch = 0; epoch < EPOCHS; ++epoch) {
+        fprintf(stderr, "Current epoch %d of %zu\n", epoch + 1, EPOCHS);
+        info_L1 buffer = get_cache_line(result.cache_size, result.associativity);
+        counter[buffer]++;
+    }
+    max_count = 0;
+    for (auto [measure, count]: counter) {
+        if (count > max_count && measure.cache_line != 0) {
             max_count = count;
             result = measure;
         }
